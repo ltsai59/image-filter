b3ef9dc0d227ccaa5256019a9c8094dc2a98c5ff
Laura Tsai <ltsai59@gmail.com>
2020-02-29 20:36:58 -0500

    Filled the TODO portion of filteredimage root end poin Modified package.json script command to accommodate Windows 10 mkdir command

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -27,16 +27,36 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/filteredimage/",  async ( req, res ) => {
+    let { image_url } = req.query;
+
+    // 1. validate the image_url query
+    if (!image_url) {
+      return res.status(400).send({ message: 'Image URL is required or malformed' });
+    }
+
+    //2. call filterImageFromURL(image_url) to filter the image
+    try {
+        let filteredPath = await filterImageFromURL(image_url);
+        //3. send the resulting file in the response
+        //4. deletes any files on the server on finish of the response
+        await res.status(200).sendFile(filteredPath, () => { deleteLocalFiles([filteredPath]); });
+    } catch (error) {
+      res.status(422).send({ message: 'Unable to process the image. Please make sure to have correct image url'} );
+    }
+
+  } );
+
 
   // Start the Server
   app.listen( port, () => {
